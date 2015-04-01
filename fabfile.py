7fae66804e3ff27910cfd86d594a0241cc6b64c7
Tyler Long <tyler4long@gmail.com>
2015-04-01 18:01:46 +0800

    Add images

--- fabfile.py
+++ fabfile.py
@@ -2,15 +2,16 @@ import re
 from subprocess import Popen, PIPE
 from fabric.api import local
 
-
 def update():
     local('rm jquery-ui.min.css')
     local('rm jquery-ui.min.js')
+    local('rm -rf images')
     output = Popen(["bower", "info", "jquery-ui"], stdout = PIPE).communicate()[0]
     latest_version = re.findall(r"version: '([1-9]\.\d+\.\d+)',", output)[0]
-    print('latest version: {0}'.format(latest_version))
     local('wget http://jqueryui.com/resources/download/jquery-ui-{0}.zip'.format(latest_version))
     local('unzip jquery-ui-{0}.zip && rm jquery-ui-{0}.zip'.format(latest_version))
     local('cp jquery-ui-{0}/jquery-ui.min.css .'.format(latest_version))
     local('cp jquery-ui-{0}/jquery-ui.min.js .'.format(latest_version))
+    local('cp -r jquery-ui-{0}/images .'.format(latest_version))
     local('rm -rf jquery-ui-{0}'.format(latest_version))
+    print('latest version: {0}'.format(latest_version))
