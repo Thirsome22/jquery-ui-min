308e474996aebe1b9615e28b94a0d9f81eab5ee0
Tyler Long <tyler4long@gmail.com>
2015-04-01 19:34:13 +0800

    Create themes folder

--- fabfile.py
+++ fabfile.py
@@ -3,13 +3,13 @@ from subprocess import Popen, PIPE
 from fabric.api import local
 
 def update():
-    local('rm jquery-ui.min.css')
+    local('rm themes/base/jquery-ui.min.css')
     local('rm jquery-ui.min.js')
-    local('rm -rf images')
+    local('rm -rf themes/base/images')
     local('bower install jquery-ui')
     local('cp bower_components/jquery-ui/jquery-ui.min.js .')
-    local('cp bower_components/jquery-ui/themes/base/jquery-ui.min.css .')
-    local('cp -r bower_components/jquery-ui/themes/base/images .')
+    local('cp bower_components/jquery-ui/themes/base/jquery-ui.min.css themes/base/')
+    local('cp -r bower_components/jquery-ui/themes/base/images themes/base/')
     local('rm -rf bower_components')
     output = Popen(["bower", "info", "jquery-ui"], stdout = PIPE).communicate()[0]
     latest_version = re.findall(r"version: '([1-9]\.\d+\.\d+)',", output)[0]
